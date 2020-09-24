60be258007d719c41b40f78fcc7614a6fd22a604
Gleb Zavyalov <56771324+zavygy@users.noreply.github.com>
2020-09-24 12:11:07 +0300

    Initial Commit

---
+++ document/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  document
+//
+//  Created by Глеб Завьялов on 24.09.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
